48bf6158306ac2dcedf485990646d6c01e1e4179
LiinaLind <78505693+LiinaLind@users.noreply.github.com>
2021-02-24 18:36:12 +0200

    Delete yl3_2.py

--- yl3_2.py
+++
@@ -1,9 +0,0 @@
-# 3.2 Jänesevanemate mure
-ring = int(input("Sisestage ringide arv: "))
-porgand = 0
-porgandeid = []
-
-for i in range(0,int(ring/2)): 
-    porgand += 2
-    porgandeid.append(int(porgand))
-print("Saadavate porgandite koguarv on " ,sum(porgandeid))
